dd850e0060fe558ce4f795bbab871bb9f99f98f5
jw <jingooo2@g.skku.edu>
2022-06-13 19:11:27 +0900

    add get_template(), make_new_audit()

--- main.py
+++ main.py
@@ -1,16 +1,126 @@
-# This is a sample Python script.
+import json
+import requests
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/documents', 'https://www.googleapis.com/auth/drive']
+
+# The ID of a sample document.
+DOCUMENT_ID = '1SyNpqms898gy1OPcaIVwNm_NC2djjAbsLQJHBGpE0WY'
+
+TEMPLATE = "./template.json"
+INPUT = "./default_input.json"
+
+def get_template():
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    try:
+        service = build('docs', 'v1', credentials=creds)
+
+        # Retrieve the documents contents from the Docs service.
+        document = service.documents().get(documentId=DOCUMENT_ID).execute()
+
+        with open(TEMPLATE, 'w') as template:
+            json.dump(document, template, indent=2)
+
+        print('The title of the document is: {}'.format(document.get('title')))
+
+    except HttpError as err:
+        print(err)
+
+def make_new_audit():
+    """Shows basic usage of the Docs API.
+    Prints the title of a sample document.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    try:
+        docs_service = build('docs', 'v1', credentials=creds)
+        drive_service = build('drive', 'v3', credentials=creds)
+
+        parents_id = ''
+        title = "[HEXAudit] DeFiContractAudit_"
+        with open(INPUT, "r") as input:
+            project_name = json.load(input)["Project-Name"]
+            print("project name:", project_name)
+
+            title += project_name + "_v1.0_KR"
+
+            audit_folder_id = "13HWHYSj5RIaN82Sut2lCBVEte8y2V80r"
+            folder_metadata = {
+                'name' : project_name,
+                'parents': [audit_folder_id],
+                'mimeType' : 'application/vnd.google-apps.folder'
+            }
+            folder_response = drive_service.files().create(body=folder_metadata).execute()
+            parents_id = folder_response.get('id')
+
+            print("new folder id:", parents_id)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+        body = {
+            'name': title
+        }
+        drive_response = drive_service.files().copy(
+            fileId=DOCUMENT_ID, body=body).execute()
+        audit_docs_id = drive_response.get('id')
+        audit_docs = drive_service.files().get(fileId=audit_docs_id,
+                                               fields='parents').execute()
 
+        previous_parents_id = ",".join(audit_docs.get('parents'))
 
-# Press the green button in the gutter to run the script.
+        audit_docs_id = drive_service.files().update(fileId=audit_docs_id,
+                                                     addParents=parents_id,
+                                                     removeParents=previous_parents_id,
+                                                     fields='id').execute()
+
+        print("new audit docs id:", audit_docs_id)
+
+    except HttpError as err:
+        print(err)
+
+def
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    #get_template()
+    make_new_audit()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
